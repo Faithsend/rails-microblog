a5493d93888aaf7213dcd32fc56f77828c6c9220
Faithsend <matt@sandfalls.com>
2015-07-05 21:16:49 -0500

    destroy dependency on user's microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token
 
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i

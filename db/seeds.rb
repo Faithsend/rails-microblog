b0f9ee3c9274d203d79247c35c9f9ac51e4a90be
Faithsend <matt@sandfalls.com>
2015-07-05 20:56:25 -0500

    admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name:  "Example User",
+User.create!(name:  "Admin User",
              email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar")
+             password:              "master",
+             password_confirmation: "master",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
